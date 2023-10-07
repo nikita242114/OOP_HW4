22dec6bfb7848d7beae99efbdcf1ce74bfa10411
nikita242114 <5358711@mail.ru>
2023-10-07 19:35:50 +0500

    work

---
+++ Apple.java
@@ -0,0 +1,9 @@
+public class Apple extends Fruit{
+   
+    public Apple(float weight) {
+        super(weight);
+    }
+    public Apple() {
+        super(1.0f); 
+    }
+}
---
+++ BoxCustomException.java
@@ -0,0 +1,5 @@
+public class BoxCustomException extends RuntimeException{
+    public BoxCustomException(String message) {
+        super(message);
+    }
+}
